022f1311f3a9e98560f1cf61c4fbdba1a35d6d51
arshu2001 <128467852+arshu2001@users.noreply.github.com>
2026-01-06 19:48:24 +0530

    ui changes

--- lib/view_models/main_view_model.dart
+++ lib/view_models/main_view_model.dart
@@ -1,6 +1,4 @@
 import 'package:get/get.dart';
-import '../views/home/home_view.dart';
-import '../views/video/video_view.dart';
 
 class MainViewModel extends GetxController {
   final RxInt currentIndex = 0.obs;
